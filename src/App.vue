6f6629ddf8ae0c6d7354e6fe2c92d167744342e8
KaustubhOP <kaustubhchaure68@gmail.com>
2025-01-24 15:51:53 +0530

    main

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import { RouterLink, RouterView } from 'vue-router'
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/about">About</RouterLink>         
         <RouterLink to="/Read">helo  world</RouterLink>
       </nav>
     </div>
